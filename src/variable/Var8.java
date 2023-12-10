de489060e7558ca9e25222fe6daa191816b3c6cb
yunaxong <songyoona1209@gmail.com>
2023-12-11 08:49:57 +0900

    test

---
+++ src/variable/Var8.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+
+        //정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // 대충 20억까지
+        long l;  // 가장 큰 정수
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0; //더 큰 범위의 실수
+
+    }
+}
